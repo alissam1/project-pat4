1dcf151ece8e20f2717dfbfb96e3b081900ab4a8
alissam1 <48532202+alissam1@users.noreply.github.com>
2019-03-14 23:01:25 +0300

    Add files via upload

---
+++ page2.sql
@@ -0,0 +1,19 @@
+
+DROP TABLE if exists `cars`;
+
+CREATE TABLE `cars` (
+  `id` mediumint(8) unsigned NOT NULL auto_increment,
+  `name` varchar(255) default NULL,
+  
+) AUTO_INCREMENT=1;
+
+INSERT INTO `type of car` (`id`,`name) VALUES 
+VALUES
+	(1,'Ferrari 812'),
+	(2,'Ford GT'),
+	(121,'Lamborghini Huracan'),
+	(3,'Porche 911'),
+	(134,'Mclaren 720s'),
+	(5,'Ferrari 488 GTB'),
+	(6,'ASton Martin DBS'),
+	(126,'Audi R8')
