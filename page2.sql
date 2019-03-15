bada8d3abebeba91c633a0fde5ef62d488009691
alissam1 <48532202+alissam1@users.noreply.github.com>
2019-03-16 01:32:31 +0300

    Update page2.sql

--- page2.sql
+++ page2.sql
@@ -3,17 +3,18 @@ DROP TABLE if exists `cars`;
 
 CREATE TABLE `cars` (
   `id` mediumint(8) unsigned NOT NULL auto_increment,
-  `name` varchar(255) default NULL,
+  `name` varchar(200) default NULL,
+  `model` varchar(255) default NULL,
   
 ) AUTO_INCREMENT=1;
 
-INSERT INTO `type of car` (`id`,`name`) VALUES 
+INSERT INTO `type of car` (`id`,`name`,`model`) VALUES 
 VALUES
-	(1,'Ferrari'),
-	(2,'Ford GT'),
-	(121,'Lamborghini Huracan'),
-	(3,'Porche 911'),
-	(134,'Mclaren 720s'),
-	(5,'Ferrari 488 GTB'),
-	(6,'ASton Martin DBS'),
-	(126,'Audi R8')
+	(`1`,'Ferrari',`812`),
+	(`2`,'Ford,`GT'),
+	(`121`,'Lamborghini Huracan', `LP 610`),
+	(`3`,'Porche`,`911'),
+	(`134`,'Mclaren`,`720s'),
+	(`4`,'Ferrari`,`488 GTB'),
+	(`6`,'Aston Martin`,`DBS'),
+	(`126`,'Audi`,`R8')
