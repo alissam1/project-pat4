93eb995aaca2266cf4b900c98772e646078d792a
alissam1 <48532202+alissam1@users.noreply.github.com>
2019-03-16 01:37:40 +0300

    Update database.php

--- database.php
+++ database.php
@@ -2,3 +2,4 @@
 $db = new PDO('mysql:host=localhost;dbname=dbalissam1','alissam1' , 'website2019winter');
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+?>
