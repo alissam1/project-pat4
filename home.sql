489985e334d9e5239ca00c12359ce3c056f34a1f
alissam1 <48532202+alissam1@users.noreply.github.com>
2019-03-16 01:36:18 +0300

    Update home.sql

--- home.sql
+++ home.sql
@@ -2,9 +2,9 @@ drop database home;
 create database home;
 home;
 
-CategoryId, Title
-10, Home
-20, Types of Car
-30, Details
-40, Review
+INSERT INTO `type of car` (`CategoryId`,`Title`) VALUES 
+(`10`, `Home`)
+(`20`, `Types of Car`)
+(`30`, `Details`)
+(`40`, `Review`)
 
